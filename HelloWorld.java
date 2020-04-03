0e3555c5f1896a880944d9cbffe76d0f7a0719e3
40660367 <30319701+40660367@users.noreply.github.com>
2020-04-03 10:59:00 +0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+   /* 这是第一个Java程序
+    * 此程序执行后将打印输出：'Hello World' 
+    */
+
+   public static void main(String []args) {
+      System.out.println("Hello World"); // 打印输出 Hello World
+   }
+}
+
